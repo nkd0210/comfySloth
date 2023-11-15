b507fb9bea217d7449e3e936e838d905f1ba41c2
Nguyen Kim Dung <kimdungvn52@gmail.com>
2023-11-15 14:21:05 +0700

    products filtered

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,8 +2,46 @@ import React from 'react'
 import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
+
 const Sort = () => {
-  return <h4>sort </h4>
+  const {
+    filtered_products: products,
+    grid_view,
+    setGridView,
+    setListView,
+    sort,
+    updateSort
+  } = useFilterContext();
+
+  return (
+    <Wrapper>
+      <div className='btn-container'>
+        <button type="button" className={`${grid_view ? 'active' : null}`} onClick={setGridView}>
+          <BsFillGridFill />
+        </button>
+        <button type="button" className={`${!grid_view ? 'active' : null}`} onClick={setListView}>
+          <BsList />
+        </button>
+      </div>
+      <p>{products.length} products found</p>
+      <hr />
+      <form>
+        <label htmlFor='sort'>sort by</label>
+        <select
+          name='sort'
+          id='sort'
+          className='sort-input'
+          value={sort}
+          onChange={updateSort}
+        >
+          <option value="price-lowest">price (lowest) </option>
+          <option value="price-highest">price (highest) </option>
+          <option value="name-a">name (a-z) </option>
+          <option value="name-z">name (z-a) </option>
+        </select>
+      </form>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
@@ -62,11 +100,20 @@ const Wrapper = styled.section`
     font-size: 1rem;
     text-transform: capitalize;
     padding: 0.25rem 0.5rem;
+    background: none;
+    cursor: pointer;
   }
+
+  .sort-input:hover {
+    opacity: 0.7;
+  }
+
   label {
+    margin-right: 0.5rem;
     font-size: 1rem;
     text-transform: capitalize;
   }
+  
 `
 
 export default Sort
