fdf06ce7698d288e9844b80315dd5caaee43ffd4
Nguyen Kim Dung <kimdungvn52@gmail.com>
2023-11-25 11:54:39 +0700

    cart complete

--- src/index.js
+++ src/index.js
@@ -11,10 +11,23 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// dev-1fsp36p77m25vn14.us.auth0.com
+// T4ZK4xmaQFaQcOyjeqfohoyPLrPAPJRp
+
 root.render(
-    <ProductsProvider>
-        <FilterProvider>
-            <App />
-        </FilterProvider>
-    </ProductsProvider>
+    <Auth0Provider
+        domain="dev-1fsp36p77m25vn14.us.auth0.com"
+        clientId="T4ZK4xmaQFaQcOyjeqfohoyPLrPAPJRp"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+        <ProductsProvider>
+            <FilterProvider>
+                <CartProvider>
+                    <App />
+                </CartProvider>
+            </FilterProvider>
+        </ProductsProvider>
+    </Auth0Provider>
 );
